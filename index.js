bd7f1d3abf56953295228138fdbb87055364891f
Bivek Baral <baralbibek1234@gmail.com>
2024-10-30 01:32:36 +0930

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// console.log(`Hello`);
+// console.log(`I like pizza`);
+
+// // window.alert(`This is an alert`);
+// // window.alert(`I like Pizza`);
+// //this is the comment
+// document.getElementById("h1").textContent = `Hello`;
+// let online = true;
+// console.log(`Bro is online:${online}`);
+
+// let forSale = false;
+// console.log(`Dis you sale it:${forSale}`);
+let fullName = "bivek";
+let age = 27;
+let student = false;
+
+document.getElementById("p1").textContent = fullName;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = student;
